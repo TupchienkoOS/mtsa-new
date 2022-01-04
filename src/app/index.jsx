95aeb82dbf00fad9419f27b20ada51f32b8355a9
Олександр Тупчієнко <tupchienkoos@gmail.com>
2022-01-04 17:11:39 +0200

    body and html changing for login

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from '../pages/login'
 
 const App = () => {
   return (
     <div className='App'> 
-    <h1 className='text-rose-900 underline '>hello world</h1>    
+      <Login />
     </div>
   )
 }
