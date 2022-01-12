7cb3566f1dd4fc49612545a85f86324213ea99e1
Олександр Тупчієнко <tupchienkoos@gmail.com>
2022-01-12 15:02:13 +0200

    routes fixed

--- src/pages/public/index.jsx
+++ src/pages/public/index.jsx
@@ -5,7 +5,7 @@ import Login from './login/login'
 const Public = () => {
     return (
         <Routes>
-            <Route path="/" element={<Navigate to="login" />} />
+            <Route path="/*" element={<Navigate to="login" />} />
             <Route path="login" element={< Login />} />
             <Route path="register" element={< Login />} />
         </Routes>
